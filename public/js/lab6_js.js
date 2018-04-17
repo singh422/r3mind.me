a3071341d05758441a0455cbf0a90fbbecfcaa53
Avinash Singh <singh422@purdue.edu>
2018-04-17 02:22:20 -0400

    Added functionality to write user data to firebase

--- public/js/lab6_js.js
+++ public/js/lab6_js.js
@@ -1,12 +1,32 @@
 var baseUrl = "http://localhost:5000/";
 
-
+var firstName;
+var lastName;
+var phoneNumber;
+var emailAddress;
+var password;
+var user = firebase.auth().currentUser;
+var database = firebase.database();
 
 function body_on_load(){
 
   loginButtonInput.onclick = onClickLoginButton;
   signupButtonInput.onclick = onClickSignupButton;
   logoutButtonInput.onclick = onClicklogoutButton;
+
+  if (user) {
+    // User is signed in.
+    loginForm.style.display = "none";
+    reminderPage.style.display = "block";
+    console.log("comes here and is successful")
+  } else {
+    // No user is signed in.
+    loginForm.style.display = "block";
+    reminderPage.style.display = "none";
+
+  }
+
+
  }
 
 
@@ -17,28 +37,29 @@ function body_on_load(){
      loginForm.style.display = "none";
      reminderPage.style.display = "block";
      console.log("comes here and is successful")
-
-
    } else {
      // No user is signed in.
 
      loginForm.style.display = "block";
      reminderPage.style.display = "none";
 
-
    }
  });
 
  function onClickSignupButton(){
 
-   var firstName = firstNameInputField.value;
-   var lastName = lastNameInputField.value;
-   var phoneNumber = phoneNumberInputField.value;
-   var emailAddress = emailInputField.value;
-   var password = passwordInputField.value;
+   firstName = firstNameInputField.value;
+   lastName = lastNameInputField.value;
+   phoneNumber = phoneNumberInputField.value;
+   emailAddress = emailInputField.value;
+   password = passwordInputField.value;
 
    //alert("Name = " + firstName + lastName +emailAddress);
-   firebase.auth().createUserWithEmailAndPassword(emailAddress, password).catch(function(error) {
+   firebase.auth().createUserWithEmailAndPassword(emailAddress, password).then(function(response) {
+      console.log(response.uid);
+      writeUserDataToDatabase(firstName,lastName,emailAddress,phoneNumber,response.uid);
+
+      }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -47,8 +68,8 @@ function body_on_load(){
  }
 
  function onClickLoginButton() {
-   var emailAddress = emailSignInInputField.value;
-   var password = passwordSignInInputField.value;
+   emailAddress = emailSignInInputField.value;
+   password = passwordSignInInputField.value;
   // alert("comes  here " + emailAddress);
 
    firebase.auth().signInWithEmailAndPassword(emailAddress, password).catch(function(error) {
@@ -59,8 +80,6 @@ function body_on_load(){
     console.log(errorMessage);
   // ...
   });
-
-
 
  }
 
@@ -74,5 +93,20 @@ function body_on_load(){
 
     // An error happened.
     });
+ }
 
+ function writeUserDataToDatabase(firstName,lastName,emailAddress,phoneNumber,userID) {
+   firebase.database().ref('users/'+userID).set({
+    firstName: firstName,
+    lastName: lastName,
+    email: emailAddress,
+    phoneNumber: phoneNumber
+    }).then(function() {
+      console.log('Synchronization succeeded');
+    })
+    .catch(function(error) {
+      console.log('Synchronization failed');
+      console.lof(error);
+    });
+    console.log("successful write");
  }
