51966521a2c6edd97a6ac286aacdbfa516db243d
Avinash Singh <singh422@purdue.edu>
2018-04-17 00:29:22 -0400

    Added firebase login

--- public/js/lab6_js.js
+++ public/js/lab6_js.js
@@ -1,24 +1,78 @@
+var baseUrl = "http://localhost:5000/";
+
+
+
 function body_on_load(){
 
   loginButtonInput.onclick = onClickLoginButton;
   signupButtonInput.onclick = onClickSignupButton;
-
+  logoutButtonInput.onclick = onClicklogoutButton;
  }
 
+
+ firebase.auth().onAuthStateChanged(function(user) {
+   if (user) {
+     // User is signed in.
+     //alert("Ho ho ho user logged in");
+     loginForm.style.display = "none";
+     reminderPage.style.display = "block";
+     console.log("comes here and is successful")
+
+
+   } else {
+     // No user is signed in.
+
+     loginForm.style.display = "block";
+     reminderPage.style.display = "none";
+
+
+   }
+ });
+
  function onClickSignupButton(){
-   alert("comes here");
+
    var firstName = firstNameInputField.value;
    var lastName = lastNameInputField.value;
    var phoneNumber = phoneNumberInputField.value;
    var emailAddress = emailInputField.value;
    var password = passwordInputField.value;
 
-   alert("Name = " + firstName + lastName +emailAddress)
-
+   //alert("Name = " + firstName + lastName +emailAddress);
+   firebase.auth().createUserWithEmailAndPassword(emailAddress, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+  });
  }
+
  function onClickLoginButton() {
    var emailAddress = emailSignInInputField.value;
-   var password = passwordInputField.value;
-   alert("comes  here " + emailAddress);
+   var password = passwordSignInInputField.value;
+  // alert("comes  here " + emailAddress);
+
+   firebase.auth().signInWithEmailAndPassword(emailAddress, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("sad coz it comes here");
+    console.log(errorMessage);
+  // ...
+  });
+
+
+
+ }
+
+ function onClicklogoutButton (){
+
+   firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    console.log("log out successful");
+
+    }).catch(function(error) {
+
+    // An error happened.
+    });
 
  }
