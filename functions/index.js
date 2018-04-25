7e4886791d8aa159deceb7df2701d7d28020f268
Stefany Go <gunawang@purdue.edu>
2018-04-25 00:49:30 -0400

    authentication/configuration

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,8 @@
+export TWILLIO_ACCOUNT_sid=AC40f97bf7971288974830ce8bf0850e12
+export TWILLIO_PHONE_NUMBER=+18178736463
+export TWILIO_AUTH_TOKEN=591995cd8367c406d6084f4120288fe5
+
+
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
